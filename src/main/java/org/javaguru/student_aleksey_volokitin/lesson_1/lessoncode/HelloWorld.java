125589d5fa28f01e88eb9120877a8b306615add3
Aleksey Volokitin <vvolfer.1985@gmail.com>
2025-04-25 04:04:36 +0900

    lesson 1

---
+++ src/main/java/org/javaguru/student_aleksey_volokitin/lesson_1/lessoncode/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_aleksey_volokitin.lesson_1.lessoncode;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+    }
+}
