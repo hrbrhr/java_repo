3d753083d0b28e71dd20eb1e3b9bdbb2412e8fc8
alexey-y-a <lexus2157@mail.ru>
2025-04-26 23:44:29 +0300

    lesson_1 homework level_6

---
+++ src/main/java/org/javaguru/student_alexey_yakovlev/lesson_1_software_setup/homework/level_6_middle/task_1/Sum.java
@@ -0,0 +1,10 @@
+package org.javaguru.student_alexey_yakovlev.lesson_1_software_setup.homework.level_6_middle.task_1;
+
+class Sum {
+
+    public static void main(String[] args) {
+        int num1 = 74;
+        int num2 = 36;
+        System.out.println(num1 + num2);
+    }
+}
