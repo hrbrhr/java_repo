f3575330633b57ca8e296c90f7dacdf5e9c17b1b
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-04-29 23:58:41 +0200

    Artjoms_babicevs

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_1/TestProgram.java
@@ -0,0 +1,16 @@
+package org.javaguru.student_artjoms_babicevs.lesson_1;
+
+class TestProgram {
+
+     public static void main(String[] args) {
+         int number1 = 23;
+         int number2 = 2434;
+
+         int sum = number1 + number2;
+         System.out.println(sum);
+
+     }
+}
+
+
+
