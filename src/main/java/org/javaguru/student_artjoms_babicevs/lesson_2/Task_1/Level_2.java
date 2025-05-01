240351a29dabe23afae7bb75fbc394d70932d557
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-01 21:55:22 +0200

    Lesson_2

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_2/Task_1/Level_2.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_artjoms_babicevs.lesson_2.Task_1;
+
+class LoginService {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+
+}
