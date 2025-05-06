178a8c432e69d11348144e0808dbc79fd11998dd
alexey-y-a <lexus2157@mail.ru>
2025-05-06 23:14:58 +0300

    hw lesson_2 level 1, 2

---
+++ src/main/java/org/javaguru/student_alexey_yakovlev/lesson_2_variables_and_data_types/homework/level_2_intern/task_1/LoginService.java
@@ -0,0 +1,8 @@
+package org.javaguru.student_alexey_yakovlev.lesson_2_variables_and_data_types.homework.level_2_intern.task_1;
+
+class LoginService {
+
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+}
