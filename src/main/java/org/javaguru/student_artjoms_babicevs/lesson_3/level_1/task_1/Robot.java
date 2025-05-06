06c5c01028383bc90d7a1f4d33420ab16eb48f88
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-06 20:43:31 +0200

    Lesson_3

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_1/task_1/Robot.java
@@ -0,0 +1,11 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_1.task_1;
+
+class Robot {
+
+    private String name;
+
+    Robot(String name) {
+        this.name = name;
+    }
+
+}
