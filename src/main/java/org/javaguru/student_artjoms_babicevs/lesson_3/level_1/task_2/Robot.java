8f8f4240cbf8b8bae7f7f6b7b054eeea7eb9f31f
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-06 20:38:33 +0200

    Lesson_2

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_1/task_2/Robot.java
@@ -0,0 +1,11 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_1.task_2;
+
+class Robot {
+
+    private String name;
+
+    Robot(String name) {
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_1/task_2/RobotApp.java
@@ -0,0 +1,10 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_1.task_2;
+
+class RobotApp {
+
+    public static void main(String[] args) {
+        Robot robot1 = new Robot("John");
+        Robot robot2 = new Robot("Michael");
+    }
+
+}
