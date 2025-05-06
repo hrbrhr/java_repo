e3c2a9a0413ff6ceba0b884b53ebad96136db627
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-06 22:01:39 +0200

    Lesson_3

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_1/task_3/RobotApp.java
@@ -0,0 +1,13 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_1.task_3;
+
+class RobotApp {
+
+    public static void main(String[] args) {
+        Robot robot1 = new Robot("John");
+        Robot robot2 = new Robot("Michael");
+
+        System.out.println("Robot 1 name = " + robot1.getName());
+        System.out.println("Robot 2 name = " + robot2.getName());
+    }
+
+}
