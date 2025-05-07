35bbfd6c0683a4fa4c75b666f2d3172bd79b6460
Aleksey Volokitin <vvolfer.1985@gmail.com>
2025-05-07 11:48:51 +0900

    fix

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_1/task_4/Robot.java
@@ -0,0 +1,14 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_1.task_4;
+
+class Robot {
+
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
