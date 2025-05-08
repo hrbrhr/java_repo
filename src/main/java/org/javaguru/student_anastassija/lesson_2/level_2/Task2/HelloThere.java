aebc2817c5e612ca58c76cd1488f7ee1e085bedb
kasar <anastassija.baturina@gmail.com>
2025-05-08 16:00:51 +0300

    Homework Lesson_2, Task2 HelloThere

---
+++ src/main/java/org/javaguru/student_anastassija/lesson_2/level_2/Task2/HelloThere.java
@@ -0,0 +1,14 @@
+package org.javaguru.student_anastassija.lesson_2.level_2.Task2;
+
+import java.util.Scanner;
+
+public class HelloThere {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter user name: ");
+        String username = scanner.nextLine();
+        System.out.println("Hello " + username);
+    }
+}
