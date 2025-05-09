8912028c7a630d677b4d751ab8d3bf94330ff3c3
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-09 22:26:27 +0200

    lesson_3

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_5/task_3/CarDemo.java
@@ -0,0 +1,10 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_5.task_3;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+
+}
