8126bbc52f0040bab3ca1cd9d7e1625fb4a7e114
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-09 22:26:10 +0200

    lesson_3

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_5/task_3/Car.java
@@ -0,0 +1,15 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_5.task_3;
+
+ class Car {
+
+        private String model;
+
+        Car(String newModel) {this.model = newModel;}
+
+        String getModel() {return this.model;}
+
+    }
+
+
+
+
