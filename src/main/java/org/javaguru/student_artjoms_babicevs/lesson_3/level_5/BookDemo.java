f71820e3e6e71528b95568b9f95ec3669f562162
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-09 21:48:18 +0200

    lesson_3

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_5/BookDemo.java
@@ -0,0 +1,20 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_5;
+
+public class BookDemo {
+
+    public static void main(String[] args) {
+        Book myBook = new Book("Principles");
+        String bookTitle = myBook.getTitle();
+        System.out.println("Book title = " + bookTitle);
+    }
+
+}
+
+
+
+
+
+
+
+
+
