99d214a9175c2b5604ce440443fcc97c4c300a35
kasar <anastassija.baturina@gmail.com>
2025-05-09 12:27:30 +0300

    Lesson2_level3_hw

---
+++ src/main/java/org/javaguru/student_anastassija/lesson_2/level_3/Task3/AverageCalculator.java
@@ -0,0 +1,23 @@
+package org.javaguru.student_anastassija.lesson_2.level_3.Task3;
+
+import java.util.Scanner;
+
+public class AverageCalculator {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please provide first number: ");
+        double first = scanner.nextDouble();
+
+        System.out.println("Please provide second number: ");
+        double second = scanner.nextDouble();
+
+        System.out.println("Please provide third number: ");
+        double third = scanner.nextDouble();
+
+        System.out.println("Arithmetical mean is " + (first + second + third)/3);
+
+    }
+}
