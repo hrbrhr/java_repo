1e496147530f54349c7fb92b11b4421352f63431
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-09 22:16:05 +0200

    lesson_3

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_5/task_2/Phone.java
@@ -0,0 +1,19 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_5.task_2;
+
+
+    class Phone {
+
+        private String model;
+
+      Phone(String newModel) {
+            this.model = newModel;
+        }
+
+        String getModel() {
+            return this.model;
+        }
+
+    }
+
+
+
