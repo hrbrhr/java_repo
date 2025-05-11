dc75ef915a107497d791be9f7d65554ec60a4b5d
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-11 19:42:04 +0200

    lesson_3

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_7/task_2/BankAccount.java
@@ -0,0 +1,26 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_7.task_2;
+
+public class BankAccount {
+
+    private String owner;
+    private int money;
+
+    public BankAccount(String owner, int moneyAmount) {
+        this.owner = owner;
+        this.money = moneyAmount;
+    }
+
+   public String getOwner() {
+        return this.owner;
+    }
+
+    public int getMoney() {
+        return this.money;
+    }
+
+}
+
+
+
+
+
