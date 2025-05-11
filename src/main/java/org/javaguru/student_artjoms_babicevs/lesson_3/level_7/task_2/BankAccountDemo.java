f67108a84cdea8d3e11c677dacf5f118766f5594
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-11 19:42:17 +0200

    lesson_3

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_3/level_7/task_2/BankAccountDemo.java
@@ -0,0 +1,19 @@
+package org.javaguru.student_artjoms_babicevs.lesson_3.level_7.task_2;
+
+public class BankAccountDemo {
+
+    public static void main(String[] args) {
+        BankAccount bankAccount = new BankAccount("Viktor", 100000);
+        String owner = bankAccount.getOwner();
+        int money = bankAccount.getMoney();
+        System.out.println("Owner = " + owner);
+        System.out.println("Money = " + money);
+    }
+
+}
+
+
+
+
+
+
