6fcd04af8310ada7c12bc9a3815f76858d17b06e
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-05-16 22:41:55 +0200

    lesson_4

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_4/level_3/MaxOffThree.java
@@ -0,0 +1,35 @@
+package org.javaguru.student_artjoms_babicevs.lesson_4.level_3;
+
+import java.util.Scanner;
+
+public class MaxOffThree {
+
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+
+        System.out.print("Enter third number: ");
+        int c = scanner.nextInt();
+
+        int  max;
+
+        if (a >= b && a >= c) {
+            max = a;
+        } else if (b >= a && b >= c) {
+            max = b;
+        } else {
+            max = c;
+        }
+
+        System.out.println("Max number: " + max);
+    }
+
+
+}
