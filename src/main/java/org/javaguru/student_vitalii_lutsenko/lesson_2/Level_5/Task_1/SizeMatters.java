e5c2a981130df3116ebbea2fca49aba2d375e11d
VITLUTUA <vitalii.lutsenko@pata.lv>
2025-05-18 19:06:41 +0300

    Commit_2

---
+++ src/main/java/org/javaguru/student_vitalii_lutsenko/lesson_2/Level_5/Task_1/SizeMatters.java
@@ -0,0 +1,11 @@
+package org.javaguru.student_vitalii_lutsenko.lesson_2.Level_5.Task_1;
+
+class SizeMatters {
+
+        public static void main(String[] args) {
+            System.out.println("Sorry, I have an errors.");
+            System.out.println("Please fix me!");
+        }
+
+}
+
