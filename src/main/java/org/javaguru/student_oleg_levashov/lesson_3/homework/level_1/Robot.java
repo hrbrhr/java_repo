e055238168bcd6a7f4c1c4489fc0bbb857e28c00
OlegLevashov <oleg.levashov@mail.ru>
2025-05-22 00:10:25 +0300

    Update Homework lesson 3 level 1

---
+++ src/main/java/org/javaguru/student_oleg_levashov/lesson_3/homework/level_1/Robot.java
@@ -0,0 +1,18 @@
+package org.javaguru.student_oleg_levashov.lesson_3.homework.level_1;
+
+class Robot {
+
+    private String name;
+
+    Robot (String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
