cc4d3910ed86af25010858e6565bde74a66e62ea
Tiutiunnikov <itiutiunnikov@gmail.com>
2025-05-21 17:18:53 +0300

    homework lesson 4 (lvl 6) v.1

---
+++ src/main/java/org/javaguru/student_elena_tiutiunnikova/lesson_4/homework/level_6_middle/Year.java
@@ -0,0 +1,50 @@
+package org.javaguru.student_elena_tiutiunnikova.lesson_4.homework.level_6_middle;
+
+import java.util.Scanner;
+
+class Year {
+    public static void main(String[] args) {
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Введите порядковый номер месяца года: ");
+        int month = scanner1.nextInt();
+
+        switch (month) {
+            case 1:
+                System.out.println("Зима");
+                break;
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+                System.out.println("Весна");
+                break;
+            case 4:
+                System.out.println("Весна");
+                break;
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+                System.out.println("Лето");
+                break;
+            case 7:
+                System.out.println("Лето");
+                break;
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+                System.out.println("Осень");
+                break;
+            case 10:
+                System.out.println("Осень");
+                break;
+            case 11:
+                System.out.println("Осень");
+                break;
+            case 12:
+                System.out.println("Зима");
+                break;
+        }
+    }
+}
