ebefdd1877a5071919b2040958a18451efbab816
javagurulv <javagurulv@gmail.com>
2025-05-22 21:53:44 +0300

    Good Names

---
+++ src/main/java/org/javaguru/student_evgenii_pashtaev/lesson_5/level_7/v2/ForengeitConverter.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_evgenii_pashtaev.lesson_5.level_7.v2;
+
+class ForengeitConverter {
+
+    double convert(Tempereture tempereture) {
+        return  (tempereture.getTemperature() - 32) * 5/9;
+    }
+
+}
---
+++ src/main/java/org/javaguru/student_evgenii_pashtaev/lesson_5/level_7/v2/Tempereture.java
@@ -0,0 +1,15 @@
+package org.javaguru.student_evgenii_pashtaev.lesson_5.level_7.v2;
+
+class Tempereture {
+
+    private double temperature;
+
+    Tempereture(double temperature){
+        this.temperature = temperature;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+}
