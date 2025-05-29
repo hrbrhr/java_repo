a480bf3460b69a52a1104b86436d8957aa3e5316
kasar <anastassija.baturina@gmail.com>
2025-05-29 22:49:26 +0300

    lesson 6 lessoncode

---
+++ src/main/java/org/javaguru/student_aleksey_volokitin/lesson_6_plus_unit_testing/lessoncode_plus/task_1_plus/EvenNumber.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_aleksey_volokitin.lesson_6_plus_unit_testing.lessoncode_plus.task_1_plus;
+
+class EvenNumber {
+    boolean isEven(int number) {
+        return (number % 2) == 0;
+    }
+}
