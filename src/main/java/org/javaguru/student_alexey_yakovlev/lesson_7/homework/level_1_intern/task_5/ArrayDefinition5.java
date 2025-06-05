47618517f0ef2a6b59d53213befce53ff4222473
alexey-y-a <lexus2157@mail.ru>
2025-06-06 00:48:17 +0300

    hw lesson_7 level_1

---
+++ src/main/java/org/javaguru/student_alexey_yakovlev/lesson_7/homework/level_1_intern/task_5/ArrayDefinition5.java
@@ -0,0 +1,8 @@
+package org.javaguru.student_alexey_yakovlev.lesson_7.homework.level_1_intern.task_5;
+
+class ArrayDefinition5 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+    }
+}
---
+++ src/main/java/org/javaguru/student_alexey_yakovlev/lesson_7/homework/level_1_intern/task_8/ArrayLength3.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_alexey_yakovlev.lesson_7.homework.level_1_intern.task_8;
+
+class ArrayLength3 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[2];
+        System.out.println(numbers.length);
+    }
+}
