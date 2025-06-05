62f866d0bb1932211cdce19882876cbf0542e14a
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-06-05 23:51:26 +0200

    lesson_7

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_7/level_1/task_2/ArrayDefinition2.java
@@ -0,0 +1,13 @@
+package org.javaguru.student_artjoms_babicevs.lesson_7.level_1.task_2;
+
+ class ArrayDefinition2 {
+
+     public static void main(String[] args) {
+             byte[] numbers = new byte[10];
+         }
+
+     }
+
+
+
+
