9e09a919a0dfafa35d7bcf22040c0c7315be6168
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-06-06 22:39:32 +0200

    lesson_7

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_7/level_1/task_3/ArrayDefinition3.java
@@ -0,0 +1,14 @@
+package org.javaguru.student_artjoms_babicevs.lesson_7.level_1.task_3;
+
+ class ArrayDefinition3 {
+     public static void main(String[] args) {
+         int[] numbers = {1, 2, 3};
+     }
+
+ }
+
+
+
+
+
+
