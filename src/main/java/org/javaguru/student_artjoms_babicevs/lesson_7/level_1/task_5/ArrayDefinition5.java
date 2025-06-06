15fa2a7b241629de3753f39ab6cfb54dca818a55
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-06-06 22:55:19 +0200

    lesson_7

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_7/level_1/task_5/ArrayDefinition5.java
@@ -0,0 +1,12 @@
+package org.javaguru.student_artjoms_babicevs.lesson_7.level_1.task_5;
+
+ class ArrayDefinition5 {
+
+     public static void main(String[] args) {
+         int[] numbers = {1, 2, 3 };
+    }
+
+
+
+
+}
---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_7/level_1/task_6/ArrayLenght1.java
@@ -0,0 +1,15 @@
+package org.javaguru.student_artjoms_babicevs.lesson_7.level_1.task_6;
+
+ class ArrayLenght1 {
+     public static void main(String[] args) {
+         int[] numbers = new int[1];
+         System.out.println(numbers[0]);
+     }
+
+ }
+
+
+
+
+
+
