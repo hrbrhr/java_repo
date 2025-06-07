01e989f9c94a3bc6a157349240bbe124dc67ead6
Jamaisky77 <babicevs.artjoms@inbox.lv>
2025-06-07 22:25:12 +0200

    lesson_7

---
+++ src/main/java/org/javaguru/student_artjoms_babicevs/lesson_7/level_2/task_3/ForLoopDefinition3.java
@@ -0,0 +1,16 @@
+package org.javaguru.student_artjoms_babicevs.lesson_7.level_2.task_3;
+
+ class ForLoopDefinition3 {
+
+     public static void main(String[] args) {
+         for (int i = 0; i < 10;i++) {
+             System.out.println(i);
+         }
+     }
+
+ }
+
+
+
+
+
