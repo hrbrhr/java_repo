92f2778c776de55a45e830a20eed8181bf5f692e
Tiutiunnikov <itiutiunnikov@gmail.com>
2025-06-08 17:56:27 +0300

    homework lesson 7 lvl 1

--- src/main/java/org/javaguru/student_elena_tiutiunnikova/lesson_7/lessoncode/Array.java
+++ src/main/java/org/javaguru/student_elena_tiutiunnikova/lesson_7/lessoncode/Array.java
@@ -2,15 +2,15 @@ package org.javaguru.student_elena_tiutiunnikova.lesson_7.lessoncode;
 
 class Array {
     public static void main(String[] args) {
-        int [] numbers = new int [5];
+        int[] numbers = new int[5];
 
-        numbers [0] = 10;
-        numbers [1] = 85;
-        numbers [2] = 4;
-        numbers [3] = 21;
-        numbers [4] = 102;
+        numbers[0] = 10;
+        numbers[1] = 85;
+        numbers[2] = 4;
+        numbers[3] = 21;
+        numbers[4] = 102;
 
-        numbers [0] = 665;
+        numbers[0] = 665;
 
         System.out.println(numbers[0]);
         System.out.println(numbers[1]);
@@ -27,9 +27,9 @@ class Array {
 
         System.out.println("Array size: " + numbers.length);
 
-        boolean [] yesNo = new boolean [100];
-        yesNo [0] = true;
-        yesNo [1] = false;
+        boolean[] yesNo = new boolean [100];
+        yesNo[0] = true;
+        yesNo[1] = false;
 
     }
 
