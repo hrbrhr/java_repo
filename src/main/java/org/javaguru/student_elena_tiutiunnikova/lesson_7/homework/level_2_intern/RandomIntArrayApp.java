771ca97733fd12ee157695e46dc6f67a0f8098ff
Tiutiunnikov <itiutiunnikov@gmail.com>
2025-06-09 11:36:27 +0300

    homework lesson 7 lvl 2

---
+++ src/main/java/org/javaguru/student_elena_tiutiunnikova/lesson_7/homework/level_2_intern/RandomIntArrayApp.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_elena_tiutiunnikova.lesson_7.homework.level_2_intern;
+
+class RandomIntArrayApp {
+    public static void main(String[] args) {
+        RandomIntArray array = new RandomIntArray();
+        int[] array1 = array.createRandomArray();
+        array.printRandomArray(array1);
+    }
+}
