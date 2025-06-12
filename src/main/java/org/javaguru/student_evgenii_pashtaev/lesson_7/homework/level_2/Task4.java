b426c1ebe9571023b8e62df69ecbdc681acc65eb
evgenka44 <132470526+evgenka44@users.noreply.github.com>
2025-06-12 19:03:41 +0300

    lesson 7

---
+++ src/main/java/org/javaguru/student_evgenii_pashtaev/lesson_7/homework/level_2/Task4.java
@@ -0,0 +1,10 @@
+package org.javaguru.student_evgenii_pashtaev.lesson_7.homework.level_2;
+
+class Task_4 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/org/javaguru/student_evgenii_pashtaev/lesson_7/homework/level_2/Task6.java
@@ -0,0 +1,10 @@
+package org.javaguru.student_evgenii_pashtaev.lesson_7.homework.level_2;
+
+class Task6 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+}
