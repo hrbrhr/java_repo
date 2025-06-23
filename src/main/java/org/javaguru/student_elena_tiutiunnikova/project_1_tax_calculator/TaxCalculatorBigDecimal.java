93f821667112738cc442f498937cac9b81ffb42a
Tiutiunnikov <itiutiunnikov@gmail.com>
2025-06-23 19:13:17 +0300

    project 1 fin

--- src/main/java/org/javaguru/student_elena_tiutiunnikova/project_1_tax_calculator/TaxCalculatorBigDecimal.java
+++ src/main/java/org/javaguru/student_elena_tiutiunnikova/project_1_tax_calculator/TaxCalculatorBigDecimal.java
@@ -5,32 +5,55 @@ import java.math.RoundingMode;
 
 class TaxCalculatorBigDecimal {
 
-//    public BigDecimal calculate(BigDecimal salary) {
-//        BigDecimal tax = new BigDecimal("0");
-//        if (salary < 0) {
-//            tax = 0;
-//        } else if (salary < 10000) {
-//            tax = calculateTaxTo10k(salary);
-//        } else if ((salary >= 10000) && (salary < 50000)) {
-//            tax = calculateTaxFrom10kTo50k(salary);
-//        } else if (salary >= 50000) {
-//            tax = calculateTaxFrom50k(salary);
-//        } else {
-//            return tax;
-//        }
-//        return tax;
-//    }
-//
-//    public BigDecimal calculateTaxTo10k (BigDecimal salary) {
-//        return (salary / 100.0) * 30;
-//    }
-//
-//    public BigDecimal calculateTaxFrom10kTo50k (BigDecimal salary) {
-//        return 3000.0 + (((salary - 10000.0) / 100) * 40);
-//    }
-//
-//    public BigDecimal calculateTaxFrom50k (BigDecimal salary) {
-//        return 19000.0 + (((salary - 50000.0) / 100) * 50);
-//    }
+    public BigDecimal calculateTax(BigDecimal salary) {
+        BigDecimal tax = BigDecimal.ZERO;
+        if (noSalary(salary)) {
+            tax = BigDecimal.ZERO;
+        } else if (isSalaryTo10k(salary)) {
+            tax = calculateTaxTo10k(salary);
+        } else if (isSalaryFrom10kTo50k(salary)) {
+            tax = calculateTaxFrom10kTo50k(salary);
+        } else if (isSalaryFrom50k(salary)) {
+            tax = calculateTaxFrom50k(salary);
+        } else {
+            return tax;
+        }
+        return tax;
+    }
+
+    private boolean noSalary (BigDecimal salary) {
+        return salary.compareTo(BigDecimal.ZERO) < 0;
+    }
+
+    private boolean isSalaryTo10k (BigDecimal salary) {
+        return salary.compareTo(new BigDecimal("10000")) < 0;
+    }
+
+    private boolean isSalaryFrom10kTo50k (BigDecimal salary) {
+        return (salary.compareTo(new BigDecimal("10000")) >= 0)
+                && (salary.compareTo(new BigDecimal("50000")) < 0);
+    }
+
+    private boolean isSalaryFrom50k (BigDecimal salary) {
+        return (salary.compareTo(new BigDecimal("50000")) >= 0);
+    }
+
+    private BigDecimal calculateTaxTo10k (BigDecimal salary) {
+        return (salary.divide(new BigDecimal("100"), 2, RoundingMode.HALF_UP))
+                .multiply(new BigDecimal("30")).setScale(2,RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calculateTaxFrom10kTo50k (BigDecimal salary) {
+        return (new BigDecimal("3000").add((((salary.subtract(new BigDecimal("10000")))
+                .divide(new BigDecimal("100"), 2, RoundingMode.HALF_UP))
+                .multiply(new BigDecimal("40"))))).setScale(2,RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calculateTaxFrom50k (BigDecimal salary) {
+        return (new BigDecimal("19000").add((((salary.subtract(new BigDecimal("50000")))
+                .divide(new BigDecimal("100"), 2, RoundingMode.HALF_UP))
+                .multiply(new BigDecimal("50"))))).setScale(2,RoundingMode.HALF_UP);
+//                19000.0 + (((salary - 50000.0) / 100) * 50);
+    }
 
 }
--- src/main/java/org/javaguru/student_elena_tiutiunnikova/project_1_tax_calculator/TaxCalculatorBigDecimalApplication.java
+++ src/main/java/org/javaguru/student_elena_tiutiunnikova/project_1_tax_calculator/TaxCalculatorBigDecimalApplication.java
@@ -5,16 +5,19 @@ import java.math.BigDecimal;
 class TaxCalculatorBigDecimalApplication {
     public static void main(String[] args) {
 
-//        TaxCalculator calculator = new TaxCalculator();
-//
-//        BigDecimal tax1 = calculator.calculateTaxTo10k(10000);
-//        System.out.println("Tax: " + tax1);
-//
-//        BigDecimal tax2 = calculator.calculateTaxFrom10kTo50k(50000);
-//        System.out.println("Tax: " + tax2);
-//
-//        BigDecimal tax3 = calculator.calculateTaxFrom50k(60000);
-//        System.out.println("Tax: " + tax3);
+        TaxCalculatorBigDecimal calculator = new TaxCalculatorBigDecimal();
+
+        BigDecimal tax1 = calculator.calculateTax(new BigDecimal("10000"));
+        System.out.println("Tax: " + tax1);
+
+        BigDecimal tax2 = calculator.calculateTax(new BigDecimal("50000"));
+        System.out.println("Tax: " + tax2);
+
+        BigDecimal tax3 = calculator.calculateTax(new BigDecimal("60000"));
+        System.out.println("Tax: " + tax3);
+
+        BigDecimal tax4 = calculator.calculateTax(new BigDecimal("-10"));
+        System.out.println("Tax: " + tax4);
     }
 
 }
