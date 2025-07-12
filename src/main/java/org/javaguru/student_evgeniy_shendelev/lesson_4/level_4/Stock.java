4030ce809647606c4e1d2d0e1450478e790bba80
Evgeniy Shendelev <sendeleve@yandex.ru>
2025-07-12 13:02:01 +0300

    lesson 4 level 4 complete

---
+++ src/main/java/org/javaguru/student_evgeniy_shendelev/lesson_4/level_4/Stock.java
@@ -0,0 +1,50 @@
+package org.javaguru.student_evgeniy_shendelev.lesson_4.level_4;
+
+class Stock {
+    private String name;
+    private int currentPrice;
+    private int minPrice;
+    private int maxPrice;
+
+    public Stock(String name, int currentPrice) {
+        this.name = name;
+        this.currentPrice = currentPrice;
+        minPrice = currentPrice;
+        maxPrice = currentPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public int getMinPrice() {
+        return minPrice;
+    }
+
+    public int getMaxPrice() {
+        return maxPrice;
+    }
+
+    void updatePrice(int newPrice) {
+        currentPrice = newPrice;
+        if (newPrice > maxPrice) {
+            maxPrice = newPrice;
+        } else if (newPrice < minPrice) {
+            minPrice = newPrice;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "\nname='" + name + '\'' +
+                ", \ncurrentPrice=" + currentPrice +
+                ", \nminPrice=" + minPrice +
+                ", \nmaxPrice=" + maxPrice +
+                '}';
+    }
+}
