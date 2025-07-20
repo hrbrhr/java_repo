8af8651c025476fc14e3d9fe4c02787befb914c9
Khvan Vasilii <hvanvasya@gmail.com>
2025-07-20 11:19:40 +0900

    Complete lessons_3 level_1 task_2

---
+++ src/main/java/org/javaguru/student_vasilii_khvan/lesson_3/level_1/RobotApp.java
@@ -0,0 +1,9 @@
+package org.javaguru.student_vasilii_khvan.lesson_3.level_1;
+
+class RobotApp {
+
+    public static void main(String[] args) {
+        Robot t800 = new Robot("T800");
+        Robot t1000 = new Robot("t1000");
+    }
+}
