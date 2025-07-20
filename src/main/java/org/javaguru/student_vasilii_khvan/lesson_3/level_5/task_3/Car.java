71161dc0f19710b4ea318abaa93b6f851fbc23d3
Khvan Vasilii <hvanvasya@gmail.com>
2025-07-21 06:31:24 +0900

    Complete lessons_3 level_5 task_3

---
+++ src/main/java/org/javaguru/student_vasilii_khvan/lesson_3/level_5/task_3/Car.java
@@ -0,0 +1,26 @@
+package org.javaguru.student_vasilii_khvan.lesson_3.level_5.task_3;
+
+class Car {
+
+    private String model;
+
+    Car(String newModel) {
+        this.model = newModel;
+    }
+
+    String getModel() {
+        return this.model;
+
+    }
+
+}
+
+class CarDemo {
+
+    public static void main(String[] args) {
+        Car myCar = new Car("Nissan");
+        String carModel = myCar.getModel();
+        System.out.println("Car model = " + carModel);
+    }
+
+}
