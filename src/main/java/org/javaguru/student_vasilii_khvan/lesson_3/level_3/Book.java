586473c5cb9cd34c86255cc8bf6770ae8f89bd3d
Khvan Vasilii <hvanvasya@gmail.com>
2025-07-20 18:07:04 +0900

    Complete lessons_3 level_3 task_1

---
+++ src/main/java/org/javaguru/student_vasilii_khvan/lesson_3/level_3/Book.java
@@ -0,0 +1,18 @@
+package org.javaguru.student_vasilii_khvan.lesson_3.level_3;
+
+public class Book {
+
+    private String title;
+    private String author;
+    private int year;
+    private Chapter chapterOne;
+    private Chapter chapterTwo;
+
+    Book(String title, String author, int year, Chapter chapterOne, Chapter chapterTwo) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.chapterOne = chapterOne;
+        this.chapterTwo = chapterTwo;
+    }
+}
---
+++ src/main/java/org/javaguru/student_vasilii_khvan/lesson_3/level_3/Chapter.java
@@ -0,0 +1,11 @@
+package org.javaguru.student_vasilii_khvan.lesson_3.level_3;
+
+public class Chapter {
+    private String title;
+    private String text;
+
+    Chapter(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+}
