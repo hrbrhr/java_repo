c255e0293fe29eb859d91328fe0d110b5bbc0672
Khvan Vasilii <hvanvasya@gmail.com>
2025-07-20 11:40:36 +0900

    Complete lessons_3 level_2 task_1

---
+++ src/main/java/org/javaguru/student_vasilii_khvan/lesson_3/level_2/Car.java
@@ -0,0 +1,14 @@
+package org.javaguru.student_vasilii_khvan.lesson_3.level_2;
+
+public class Car {
+
+    private String brand;
+    private String color;
+    private int price;
+
+    Car(String brand, String color, int price) {
+       this.brand = brand;
+       this.color = color;
+       this.price = price;
+    }
+}
