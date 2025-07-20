101fc8b4bd145263409d4888fac51805c7fb24e7
Khvan Vasilii <hvanvasya@gmail.com>
2025-07-20 11:40:56 +0900

    Complete lessons_3 level_2 task_2

---
+++ src/main/java/org/javaguru/student_vasilii_khvan/lesson_3/level_2/CarApp.java
@@ -0,0 +1,10 @@
+package org.javaguru.student_vasilii_khvan.lesson_3.level_2;
+
+public class CarApp {
+
+    public static void main(String[] args) {
+        Car chevrolet = new Car("Chevrolet", "White", 1000);
+        Car kia = new Car("KIA", "Black", 2000);
+        Car bmw = new Car("BMW", "Blue", 3000);
+    }
+}
