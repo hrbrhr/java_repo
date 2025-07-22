86b4c14bbe2e33ca3b00c3bd1a597df145c4d17c
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-22 13:45:04 +0300

    Completed HW1

---
+++ src/main/java/org/javaguru/student_grigorii_emelyanov/lesson_1_software_setup/homework/level_6_middle/Divide.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_grigorii_emelyanov.lesson_1_software_setup.homework.level_6_middle;
+
+public class Divide {
+    public static void main(String[] args) {
+        System.out.println(50 / 3);
+    }
+}
