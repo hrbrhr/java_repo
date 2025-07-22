037843ffdf798d7a9dded72f4c47b99bca6d57be
Grigoriy Emiliyanov <egrigoriy@gmail.com>
2025-07-22 13:10:01 +0300

    Add folder for new student

---
+++ src/main/java/org/javaguru/student_grigorii_emelyanov/lesson_1_software_setup/lessoncode/HelloWorld.java
@@ -0,0 +1,7 @@
+package org.javaguru.student_grigorii_emelyanov.lesson_1_software_setup.lessoncode;
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
